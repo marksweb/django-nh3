3ca2e514ecf5d429e9d33ec973e73be2cf7c354b
Mark Walker <mwalker@ismgames.com>
2023-03-02 10:26:02 +0000

    Ran ruff fixes

--- src/django_nh3/forms.py
+++ src/django_nh3/forms.py
@@ -1,29 +1,29 @@
-from typing import Callable, Any, Optional, Set, Dict
-
-from django import forms
-from django.utils.safestring import mark_safe
+from collections.abc import Callable
+from typing import Any, Dict, Optional, Set
 
 import nh3
+from django import forms
+from django.utils.safestring import mark_safe
 
 
 class Nh3Field(forms.CharField):
     """ nh3 form field """
     empty_values = [None, "", [], (), {}]
 
-    def __init__(self, attributes: Dict[str, Set[str]] = dict(),
+    def __init__(self, attributes: Dict[str, Set[str]] = {},
                  attribute_filter: Optional[Callable[[str, str, str], Optional[str]]] = None,
-                 clean_content_tags: Set[str] = set(), link_rel: str = '',
+                 clean_content_tags: Set[str] = set(), link_rel: str = "",
                  strip_comments: bool = False, tags: Set[str] = set(), *args, **kwargs):
 
         super().__init__(*args, **kwargs)
 
         self.nh3_options = {
-            'attributes': attributes,
-            'attribute_filter': attribute_filter,
-            'clean_content_tags': clean_content_tags,
-            'link_rel': link_rel,
-            'strip_comments': strip_comments,
-            'tags': tags
+            "attributes": attributes,
+            "attribute_filter": attribute_filter,
+            "clean_content_tags": clean_content_tags,
+            "link_rel": link_rel,
+            "strip_comments": strip_comments,
+            "tags": tags
         }
 
     def to_python(self, value) -> Any:
