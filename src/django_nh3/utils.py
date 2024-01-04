2228cdc17efc67f8d9d474a56847daf00f4d2367
Christian Clauss <cclauss@me.com>
2024-01-04 22:11:49 +0100

    utils.py: Fix typo (#24)

--- src/django_nh3/utils.py
+++ src/django_nh3/utils.py
@@ -17,10 +17,10 @@ def get_nh3_default_options() -> dict[str, Any]:
         BLEACH_ALLOWED_ATTRIBUTES   -> NH3_ALLOWED_ATTRIBUTES
         BLEACH_STRIP_COMMENTS       -> NH3_STRIP_COMMENTS
 
-    While other settings are have no current support in nh3:
+    While other settings have no current support in nh3:
 
         BLEACH_ALLOWED_STYLES       -> There is no support for styling
-        BLEACH_ALLOWED_PROTOCOLS    -> There is no suport for protocols
+        BLEACH_ALLOWED_PROTOCOLS    -> There is no support for protocols
         BLEACH_STRIP_TAGS           -> This is the default behavior of nh3
 
     """
